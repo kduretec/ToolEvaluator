4c7e0a3832483d14c81c52fc001b83f9820e00c8
Krešimir Đuretec <kresimir.duretec@gmail.com>
2016-11-27 17:24:35 +0100

    improving WER class and test and updating JUnit

--- src/main/java/org/benchmarkdp/toolevaluator/utils/WordErrorRate.java
+++ src/main/java/org/benchmarkdp/toolevaluator/utils/WordErrorRate.java
@@ -19,10 +19,40 @@ public class WordErrorRate {
 	private int substitution;
 
 	private int insertion;
-	
+
+	private int correct;
+
 	private int totalOp;
 
-	private int[][] mat;
+	private class MatEl {
+		public int totalOp;
+
+		public int deletion;
+
+		public int substitution;
+
+		public int insertion;
+
+		public int correct;
+
+		public MatEl(int tO, int del, int sub, int ins, int cor) {
+			totalOp = tO;
+			deletion = del;
+			substitution = sub;
+			insertion = ins;
+			correct = cor;
+		}
+
+		public MatEl(MatEl me) {
+			totalOp = me.totalOp;
+			deletion = me.deletion;
+			substitution = me.substitution;
+			insertion = me.insertion;
+			correct = me.correct;
+		}
+	}
+
+	private MatEl[][] mat;
 
 	public WordErrorRate(String ref, String n) {
 		reference = ref;
@@ -34,14 +64,13 @@ public class WordErrorRate {
 		String[] nWords = newString.split(" ");
 		int m = rWords.length + 1;
 		int n = nWords.length + 1;
-		mat = new int[m][n];
-
+		mat = new MatEl[m][n];
 		for (int i = 0; i < m; i++) {
-			mat[i][0] = i;
+			mat[i][0] = new MatEl(i, i, 0, 0, 0);
 		}
 
 		for (int j = 0; j < n; j++) {
-			mat[0][j] = j;
+			mat[0][j] = new MatEl(j, 0, 0, j, 0);
 		}
 
 		for (int j = 1; j < n; j++) {
@@ -51,28 +80,57 @@ public class WordErrorRate {
 					subCost = 1;
 				}
 				int value = 0;
-				if (mat[i - 1][j] + 1 < mat[i][j - 1] + 1) {
-					if (mat[i - 1][j] + 1 < mat[i - 1][j - 1] + subCost) {
-						value = mat[i - 1][j] + 1;
+				if (mat[i - 1][j].totalOp + 1 < mat[i][j - 1].totalOp + 1) {
+					if (mat[i - 1][j].totalOp + 1 < mat[i - 1][j - 1].totalOp + subCost) {
+						mat[i][j] = new MatEl(mat[i - 1][j]);
+						mat[i][j].totalOp += 1;
+						mat[i][j].deletion += 1;
 					} else {
-						value = mat[i - 1][j - 1] + subCost;
+						mat[i][j] = new MatEl(mat[i - 1][j - 1]);
+						mat[i][j].totalOp += subCost;
+						mat[i][j].substitution += subCost > 0 ? 1 : 0;
+						mat[i][j].correct += subCost > 0 ? 0 : 1;
 					}
 				} else {
-					if (mat[i][j - 1] + 1 < mat[i - 1][j - 1] + subCost) {
-						value = mat[i][j - 1] + 1;
+					if (mat[i][j - 1].totalOp + 1 < mat[i - 1][j - 1].totalOp + subCost) {
+						mat[i][j] = new MatEl(mat[i][j-1]);
+						mat[i][j].totalOp += 1;
+						mat[i][j].insertion += 1;
 					} else {
-						value = mat[i - 1][j - 1] + subCost;
+						mat[i][j] = new MatEl(mat[i - 1][j - 1]);
+						mat[i][j].totalOp += subCost;
+						mat[i][j].substitution += subCost > 0 ? 1 : 0;
+						mat[i][j].correct += subCost > 0 ? 0 : 1;
 					}
 				}
-				mat[i][j] = value;
 			}
 		}
-	
-		totalOp = mat[m-1][n-1];
-		
+
+		totalOp = mat[m - 1][n - 1].totalOp;
+		deletion = mat[m - 1][n - 1].deletion;
+		substitution = mat[m - 1][n - 1].substitution;
+		insertion = mat[m - 1][n - 1].insertion;
+		correct = mat[m - 1][n - 1].correct;
+
 	}
-	
+
 	public int getTotalOp() {
 		return totalOp;
+	}
+
+	public int getInsertion() {
+		return insertion;
+	}
+
+	public int getSubstitution() {
+		return substitution;
+	}
+
+	public int getDeletion() {
+		return deletion;
+	}
+
+	public int getCorrect() {
+		return correct;
 	}
 }
--- src/test/java/org/benchmarkdp/toolevaluator/utils/WordErrorRateTest.java
+++ src/test/java/org/benchmarkdp/toolevaluator/utils/WordErrorRateTest.java
@@ -4,16 +4,45 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class WordErrorRateTest {
 
+	private void checkStrings(String s1, String s2, int o, int c, int s, int i, int d) {
+		WordErrorRate wer = new WordErrorRate(s1, s2);
+		wer.evaluate();
+		int op = wer.getTotalOp();
+		int correct = wer.getCorrect();
+		int substitution = wer.getSubstitution();
+		int insertion = wer.getInsertion();
+		int deletion = wer.getDeletion();
+
+//		System.out.println("operation=" + op + " correct=" + correct + " substitution=" + substitution + " insertion="
+//				+ insertion + " deletion=" + deletion);
+
+		assertTrue(op == o);
+		assertTrue(correct == c);
+		assertTrue(substitution == s);
+		assertTrue(insertion == i);
+		assertTrue(deletion == d);
+	}
+	
 	@Test
 	public void Test1() {
 		String s1 = "This is a nice string";
 		String s2 = "This a nice string";
-		WordErrorRate wer = new WordErrorRate(s1, s2);
-		wer.evaluate();
-		int op = wer.getTotalOp();
-		assertTrue(op==1);
+		checkStrings(s1, s2, 1, 4, 0, 0, 1);
+	}
+	
+	@Test
+	public void Test2() {
+		String s1 = "This is a nice string";
+		String s2 = "This a nice string indeed";
+		checkStrings(s1, s2, 2, 4, 0, 1, 1);
+	}
+	
+	@Test
+	public void Test3() {
+		String s1 = "P1 P2 T1 P3";
+		String s2 = "P1 P2 P3 T1";
+		checkStrings(s1, s2, 2, 2, 2, 0, 0);
 	}
 }
